a7bd2a3917779f55d15bd25938ecf1450e31a192
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-07-22 12:49:45 +0400

    sdasadass

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,13 +2,27 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
-        switch (year % 4) {
+        String result;
+        switch (year % 400) {
             case 0:
-                System.out.println("not leap");
+                result = "leap";
                 break;
             default:
-                System.out.println("leap");
-                break;
+                switch (year % 100) {
+                    case 0:
+                        result = "not leap";
+                        break;
+                    default:
+                        switch (year % 4) {
+                            case 0:
+                                result = "leap";
+                                break;
+                            default:
+                                result = "not leap";
+                                break;
+                        }
+                }
         }
+        System.out.println(result);
     }
 }
